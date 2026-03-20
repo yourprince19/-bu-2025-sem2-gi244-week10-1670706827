a05a1b036f020afbd682fa3160e52129bc7bdcc1
yourprince19 <kittakornroochen@gmail.com>
2026-03-20 16:12:46 +0700

    16.12

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,72 +1,144 @@
 using UnityEngine;
+
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
+
 {
+
     public float jumpForce;
+
     public float gravityModifier;
+
     public ParticleSystem explosionParticle;
+
     public ParticleSystem dirtParticle;
 
     public AudioClip jumpSfx;
+
     public AudioClip crashSfx;
 
     private Rigidbody rb;
+
     private InputAction jumpAction;
+
     private bool isOnGround = true;
 
+    private bool canDoubleJump = false;
+
     private Animator playerAnim;
+
     private AudioSource playerAudio;
 
     public bool gameOver = false;
 
     void Awake()
+
     {
+
         rb = GetComponent<Rigidbody>();
+
         playerAnim = GetComponent<Animator>();
+
         playerAudio = GetComponent<AudioSource>();
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
+
     {
+
         Physics.gravity *= gravityModifier;
 
         jumpAction = InputSystem.actions.FindAction("Jump");
 
         gameOver = false;
+
     }
 
     // Update is called once per frame
+
     void Update()
+
     {
-        if (jumpAction.triggered && isOnGround && !gameOver)
+
+        if (jumpAction.triggered && !gameOver)
+
         {
-            rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
-            isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig");
-            dirtParticle.Stop();
-            playerAudio.PlayOneShot(jumpSfx);
+
+            if (isOnGround)
+
+            {
+
+                rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+
+                isOnGround = false;
+
+                canDoubleJump = true;
+
+                playerAnim.SetTrigger("Jump_trig");
+
+                dirtParticle.Stop();
+
+                playerAudio.PlayOneShot(jumpSfx);
+
+            }
+
+            else if (canDoubleJump)
+
+            {
+
+                rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+
+                canDoubleJump = false;
+
+                playerAnim.SetTrigger("Jump_trig");
+
+                playerAudio.PlayOneShot(jumpSfx);
+
+            }
+
         }
+
     }
 
     private void OnCollisionEnter(Collision collision)
+
     {
+
         if (collision.gameObject.CompareTag("Ground"))
+
         {
+
             isOnGround = true;
+
             dirtParticle.Play();
+
         }
+
         else if (collision.gameObject.CompareTag("Obstacle"))
+
         {
+
             Debug.Log("Game Over!");
+
             gameOver = true;
+
             playerAnim.SetBool("Death_b", true);
+
             playerAnim.SetInteger("DeathType_int", 1);
+
             explosionParticle.Play();
+
             dirtParticle.Stop();
+
             playerAudio.PlayOneShot(crashSfx);
+
         }
+
     }
 
 }
+
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
+
 {
-    public GameObject obstaclePrefab;
+
+    public GameObject[] obstaclePrefab;
+
     public Vector3 spawnPos = new(25, 0, 0);
 
     public float startDelay = 2;
+
     public float repeatRate = 2;
 
     private PlayerController playerController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
+
     {
+
         // Instantiate(obstaclePrefab, new Vector3(25, 0, 0), obstaclePrefab.transform.rotation);
 
         InvokeRepeating(nameof(SpawnObstacle), startDelay, repeatRate);
 
-        GameObject.Find("Player").GetComponent<PlayerController>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
     }
 
     void SpawnObstacle()
+
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+
+        int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+
+        GameObject selectedObstacle = obstaclePrefab[obstacleIndex];
+
+        Instantiate(selectedObstacle, spawnPos, selectedObstacle.transform.rotation);
+
     }
+
 }
